31547e07f664d1e00a9b249d7f82db43b29f044d
Cyril ESCLASSAN <cyril.esclassan@gmail.com>
2018-12-23 22:09:51 +0100

    Un peu de code...

--- headers/squelette.h
+++ headers/squelette.h
@@ -1,5 +1,6 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 
 // mettre des choses avant
 #define MIN(a,b) (((a)<(b))?(a):(b))
@@ -33,18 +34,18 @@ typedef struct s_arc_p{ /// arc pondéré
 } t_arc_p;
 
 int * creer_tab_int(int dim); // juste un malloc de dim cases
-int ** creer_mat_int(int nbRows, int nbCol); // malob 2D (malloc maybe...)
+int ** creer_mat_int(int nbRows, int nbCols); // malob 2D (malloc maybe...)
 void affiche_tab_int(int *tab, int dim, FILE *logfp);
 
-void creer_t_mat_int_dyn(t_mat_int_dyn *stTab, int nbRows, int nbCol); // utilise la struct
+void creer_t_mat_int_dyn(t_mat_int_dyn *stTab, int nbRows, int nbCols); // utilise la struct
 void creer_t_tab_int_dyn(t_tab_int_dyn *stTab, int dim); // idem
 void creer_t_mat_char_dyn(t_mat_char_star_dyn * s_tabmots);
 void affiche_t_tab_int_dyn(t_tab_int_dyn t_tab, FILE *logfp);
 void affiche_t_mat_char_star_dyn(t_mat_char_star_dyn t_tabmots, FILE *logfp);
 void affiche_t_mat_int_dyn(t_mat_int_dyn t_tab, FILE *logfp);
-void affiche_mat_int(int **duels_mat, int nbRows, int nbCol, FILE *logfp);
+void affiche_mat_int(int **duels_mat, int nbRows, int nbCols, FILE *logfp);
 
 void init_tab_int(int *tab, int dim, int valeur);// initialise avec une valeur
-void init_mat_int(int **mat, int nbRows, int nbCol, int valeur); // idem
+void init_mat_int(int **mat, int nbRows, int nbCols, int valeur); // idem
 
 // mettre des choses après
--- sources/squelette.c
+++ sources/squelette.c
@@ -10,3 +10,17 @@
 
 #include "../headers/squelette.h"
 
+int * creer_tab_int(int dim) {
+    int * i;
+    i = malloc(dim*sizeof(int));
+    return i;
+}
+
+int ** creer_mat_int(int nbRows, int nbCols) {
+    int ** i;
+    i = malloc(nbRows*sizeof(int *));
+    for(int c = 0; c < nbRows; c++) {
+        i[c] = malloc(nbCols*sizeof(int));
+    }
+    return i;
+}
