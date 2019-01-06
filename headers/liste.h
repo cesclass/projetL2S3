46d7fc4edd38a0043b6cc21b4aa8f7be2a600b1b
Cyril ESCLASSAN <cyril.esclassan@gmail.com>
2019-01-06 12:35:08 +0100

    Reorganisation autour de liste.h

--- headers/liste.h
+++ headers/liste.h
@@ -1,16 +1,35 @@
-/* liste statique */
-/* Vincent Dugat 2018 */
+/**
+ * @file liste.h
+ * @author Vincent Dugat and Cyril E (cyril.esclassan@univ-tlse3.fr)
+ * 
+ * @brief 
+ * 
+ * @licence MIT
+ * 
+ */
 
 #ifndef listeSTAT
 #define listeSTAT
 
-#include "elementliste.h"
-#define DIMMAX 100 // taille maximale de la liste statique
-#define VIDE -1   // constante  pour indiquer que la liste est vide (par convention)
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define DIMMAX 100  // taille maximale de la liste statique
+#define VIDE -1     // constante  pour indiquer que la liste est vide (par convention)
 
 /* ----------------------------------
         Déclaration des types
    --------------------------------*/
+
+typedef struct s_arc_p { /// arc pondéré
+  int orig;
+  int dest;
+  int poids;
+} t_arc_p;
+
+typedef t_arc_p Elementliste;
 
 struct liste {
     Elementliste Tabliste[DIMMAX];
@@ -19,6 +38,12 @@ struct liste {
 };
 
 typedef struct liste liste;
+
+void saisie_element(Elementliste *e);
+void copie_element(Elementliste *cible, Elementliste source);
+void afficher_element(Elementliste e,FILE *fp);
+bool cmp_elements(Elementliste e1,Elementliste e2);
+
 int nbEltList(liste lst); // nombre d'éléments
 void createList(liste *p); // renvoie une liste statique
 void addFrontList(liste *p, Elementliste e); // ajoute un élément devant
